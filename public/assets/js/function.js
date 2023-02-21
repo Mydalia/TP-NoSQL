f48d15acdd2a6f0be29fed81df9338cf82f31f7d
Mydalia <77779115+Mydalia@users.noreply.github.com>
2023-02-21 10:17:35 +0100

    Create function.js

---
+++ public/assets/js/function.js
@@ -0,0 +1,7 @@
+const sgbdNumberOfUsers = document.getElementById('sgbd-number-of-users');
+const sgbdNumberOfProducts = document.getElementById('sgbd-number-of-products');
+const NoSqlNumberOfUsers = document.getElementById('nosql-number-of-users');
+const NoSqlNumberOfProducts = document.getElementById('nosql-number-of-products');
+
+
+
